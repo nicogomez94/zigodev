fdad64e1491d7fd26777b632e6c997d65dda05c3
nicog <nicolasgomez94@gmail.com>
2026-05-28 21:17:52 -0300

    ig

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWaveSquare, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Header: React.FC = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -30,6 +31,18 @@ const Header: React.FC = () => {
                         <li><Link to="/servicios" onClick={() => setMenuOpen(false)}>Servicios</Link></li>
                         {/* <li><Link to="/cotizador" onClick={() => setMenuOpen(false)}>Cotizador</Link></li> */}
                         <li><Link to="/trabajos" onClick={() => setMenuOpen(false)}>Portfolio</Link></li>
+                        <li>
+                            <a
+                                href="https://www.instagram.com/zigodev_agency/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="nav-social-link"
+                                aria-label="Instagram de Zigo Dev"
+                                onClick={() => setMenuOpen(false)}
+                            >
+                                <FontAwesomeIcon icon={faInstagram} />
+                            </a>
+                        </li>
                         {/* <li><Link to="/acerca-de" onClick={() => setMenuOpen(false)}>Acerca de</Link></li> */}
                         <li><Link to="https://wa.me/5491152291994" className='cotiza-mobile cta-button hero-cta-main' onClick={() => setMenuOpen(false)}>Ponete en Contacto</Link></li>
                     </ul>
